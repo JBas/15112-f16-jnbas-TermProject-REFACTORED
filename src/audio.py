4e85603bca15dab5876699fd42866bbd5623e0cb
JBas <joshua.n.bas@gmail.com>
2020-12-01 21:16:44 -0500

    -- added a notes dictionary for future matching

--- src/audio.py
+++ src/audio.py
@@ -1,5 +1,5 @@
 import time
-import scipy.signal
+from scipy import signal
 import numpy as np
 import wave
 import pyaudio
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 class Audio():
 
     RATE = 44100
-    CHANNELS = 2
+    CHANNELS = 1
     FORMAT = pyaudio.paInt16
     CHUNK = 1024
 
@@ -32,7 +32,10 @@ class Audio():
     def callback_record():
 
         def callback(in_data, frame_count, time_info, status):
-
+            data = np.fromstring(in_data, dtype=np.int16)
+            b, a = signal.butter(4, 0.1, analog=False, fs=Audio.RATE)
+            filtered = signal.filtfilt(b, a, data)
+            print(filtered)
             return (None, pyaudio.paContinue)
 
         stream = Audio.p.open(Audio.RATE,
@@ -52,13 +55,15 @@ class Audio():
     def display(seconds):
         buffers = Audio.record(seconds)
 
-        time = np.asarray(range(Audio.RATE//Audio.CHUNK * seconds))
-
+        time = np.arange(Audio.RATE//Audio.CHUNK * seconds)
         # channel are interleaved and not separable (?)
         # so can't really show interesting data with multiple channels
         amplitude = np.fromstring(buffers, dtype=np.int16)
+        time = np.arange(len(amplitude)) / Audio.RATE
+        b, a = signal.butter(1, 100, analog=False, fs=Audio.RATE)
+        filtered = signal.filtfilt(b, a, amplitude)
         fig, ax = plt.subplots()
-        ax.plot(amplitude)
+        ax.plot(time, amplitude)
         plt.show()
         return
 
--- src/sheet.py
+++ src/sheet.py
@@ -1,9 +1,37 @@
 import pyglet
+from pyglet.shapes import Line
+import json
+
+class Staff():
+
+    __LINES = 5
+    __LINE_WIDTH = 1
+    __LINE_SPACE = 5
+
+    def __init__(self, x, y, width, batch=None):
+        self.__width = width
+        self.__x = x # horizontally centered
+        self.__y = y # from top
+        self.__batch = batch
+
+        self.__lines = []
+        for i in range(Staff.__LINES):
+            self.__lines.append(Line(x, y - i*Staff.__LINE_SPACE, x+width, y - i*Staff.__LINE_SPACE, color=(0, 0, 0), batch=self.__batch))
+
 
 class Sheet(pyglet.window.Window):
 
+    __LOADED = False
+
+    __BASE_NOTES = {}
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        if (not Sheet.__LOADED):
+            with open("notes.json") as f:
+                Sheet.__BASE_NOTES = json.load(f)
+            Sheet.__LOADED = True
 
         self.batch = pyglet.graphics.Batch()
 
@@ -13,9 +41,15 @@ class Sheet(pyglet.window.Window):
                                   x=self.width//2, y=self.height,
                                   anchor_x="center", anchor_y="top",
                                   color=(0, 0, 0, 255), batch=self.batch)
-        line = pyglet.shapes.Line(0, self.height//2, self.width, self.height//2, width=1, batch=self.batch)
+
+        self.notes = []
+
+        self.staff = Staff(50, self.height//2, self.width//3, batch=self.batch)
 
         pass
+
+    def addNote(note):
+        self.notes.append(note)
 
     def loadAssets():
         pass
